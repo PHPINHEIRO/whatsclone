1923f71b79e3e069c512064adda18a4a3e92c22f
Pedro Henrique <phgp25@gmail.com>
2019-03-21 10:45:30 -0300

    configured firebase

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import firebase from '@firebase/app'
 
 import Routes from './src/Routes';
 import reducers from './src/reducers'
+import {config} from './firebaseConfig'
 
 export default class App extends Component {
+
+  componentWillMount(){
+    firebase.initializeApp(config);
+  }
   render() {
     return (
       <Provider store={createStore(reducers)}>
